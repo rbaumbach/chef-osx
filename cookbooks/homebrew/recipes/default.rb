c07c44c48dd7d5510fa0adefd99b564f6d1a7451
rbaumbach <rbaumbach.github@gmail.com>
2015-03-07 14:05:06 -0800

    Add homebrew install script

--- cookbooks/homebrew/recipes/default.rb
+++ cookbooks/homebrew/recipes/default.rb
@@ -1,5 +1,8 @@
-#
-# Cookbook Name:: homebrew
-# Recipe:: default
-#
-# Copyright (c) 2015 The Authors, All Rights Reserved.
+execute "install homebrew" do
+  command "echo | ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
+  not_if { File.exist? '/usr/local/bin/brew' }
+end
+
+execute "doctor brew" do
+  command "brew doctor"
+end
