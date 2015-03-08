5c19a43f30ec721f0a74a43880df56f03f06d70d
rbaumbach <rbaumbach.github@gmail.com>
2015-03-07 16:58:11 -0800

    Update homebrew cookbook

--- cookbooks/homebrew/recipes/default.rb
+++ cookbooks/homebrew/recipes/default.rb
@@ -1,13 +1,18 @@
-homebrew_install_directory = "/usr/local"
+homebrew_repo = 'git clone https://github.com/Homebrew/homebrew.git'
+homebrew_install_directory = '/usr/local'
+homebrew_bin = '/usr/local/bin/brew'
 
 directory homebrew_install_directory do
   user "root"
   group "admin"
+  not_if { Directory.exists? homebrew_install_directory }
 end
 
 execute "install homebrew" do
-  command "git clone https://github.com/Homebrew/homebrew.git #{homebrew_install_directory}"
-  not_if { File.exist? '/usr/local/bin/brew' }
+  command "#{homebrew_repo} #{homebrew_install_directory}"
+  user ENV['SUDO_USER']
+  group "admin"
+  not_if { File.exist? homebrew_bin }
 end
 
 execute "doctor brew" do
