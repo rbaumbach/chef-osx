88d11a51a28d298df903206a36088534d99af966
rbaumbach <rbaumbach.github@gmail.com>
2015-03-22 13:42:03 -0700

    Use git resource for homebrew, and add brew doctor

--- cookbooks/homebrew/recipes/default.rb
+++ cookbooks/homebrew/recipes/default.rb
@@ -3,19 +3,23 @@ directory '/usr/local' do
   mode '0775'
 end
 
-execute "install homebrew" do
-  command 'git clone https://github.com/Homebrew/homebrew.git /usr/local'
-  # cwd '/usr/local'
-  group 'admin'
-  user ENV['SUDO_USER']
-  not_if { File.exist? '/usr/local/bin/brew' }
+# execute "install homebrew" do
+#   command 'git clone https://github.com/Homebrew/homebrew.git /usr/local'
+#   group 'admin'
+#   user node['homebrew']['user']
+#   not_if { File.exist? '/usr/local/bin/brew' }
+# end
+
+git '/usr/local' do
+  repository 'https://github.com/Homebrew/homebrew.git'
+  user node['homebrew']['user']
+  group node['homebrew']['group']
 end
-#
-# execute "change owner" do
-#   command "chown -R sprout-user /usr/local/"
-# end
-#
-# execute "doctor brew" do  # whole resource has issues.....
-#   command "brew doctor"
-#   user ENV['SUDO_USER']
-# end
+
+execute "brew doctor" do
+  command "brew doctor"
+  user node['homebrew']['user']
+  # there is no way to ignore warnings thrown by brew doctor, will nearly
+  # always return a 1 and will cause chef failures
+  returns [0, 1]
+end
